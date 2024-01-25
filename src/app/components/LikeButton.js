33ed0068540a03adf26aca75a99feadbd5627d3f
Dev-Corgi <bora5242@gmail.com>
2024-01-26 03:18:44 +0900

    convert

--- src/app/components/LikeButton.js
+++ src/app/components/LikeButton.js
@@ -21,7 +21,7 @@ const LikeButton = ({eventId}) => {
     setisLiked(currentLiked);
         try {
           if(currentLiked){
-          await fetch(`http://localhost:8080/api/v1/heart/${eventId}`, {
+          await fetch(`https://www.damoacon.shop/api/v1/heart/${eventId}`, {
             method: "POST",
             headers: {
               "Content-Type": "application/json; charset=utf-8",
@@ -29,7 +29,7 @@ const LikeButton = ({eventId}) => {
             },
           })}
           else{
-            await fetch(`http://localhost:8080/api/v1/heart/${eventId}`, {
+            await fetch(`https://www.damoacon.shop/api/v1/heart/${eventId}`, {
               method: "DELETE",
               headers: {
                 "Content-Type": "application/json; charset=utf-8",
